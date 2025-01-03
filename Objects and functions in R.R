8fbb8975afc3d90a233ab112ec0d6a84c4889c57
Sumbati10 <lindashirasala@gmail.com>
2025-01-03 11:22:01 +0300

    functions and objectsin R

---
+++ Objects and functions in R.R
@@ -0,0 +1,28 @@
+#Objects and functions in R
+1+2
+
+5*10
+
+(3+5)/9
+
+
+#Objects are created through functions 
+#Objects are named data structures that store data
+# An object can be a single digit, a character, a boolean, a dataframe , a list of frames and etc
+
+
+#Creating an object
+#object name<-value
+
+bob <- 7
+bob
+bob*10
+print(bob)
+
+
+a<-1:10
+print(a)
+
+a*5
+
+
